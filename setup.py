a1c22605a0b48c0bf9c451585f90df53845e994b
tuxedoar <tuxedoar@gmail.com>
2019-06-17 14:41:58 -0300

    Struggling with packaging.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author='tuxedoar',
     author_email='tuxedoar@gmail.com',
     packages=['quackmyip'],
-    scripts=['quackmyip/quackmyip'],
+    scripts=['quackmyip/quackmyip.py'],
     install_requires=[
     'urllib3',
     'requests'
